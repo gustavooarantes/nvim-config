3b7c16ea0a7aeab5e1a8c6dd0829d5d566ac4b5e
Gustavo <profissional.gustavoa@gmail.com>
2025-08-10 15:06:22 -0300

    first time nvim configs - mgot jdtls working with jdk 21 so remember that in the future

---
+++ lua/config/keymaps.lua
@@ -0,0 +1,23 @@
+-- Keymaps are automatically loaded on the VeryLazy event
+-- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
+-- Add any additional keymaps here
+-- Disable arrow keys in normal mode
+vim.keymap.set("n", "<Up>", "<Nop>", { noremap = true, silent = true })
+vim.keymap.set("n", "<Down>", "<Nop>", { noremap = true, silent = true })
+vim.keymap.set("n", "<Left>", "<Nop>", { noremap = true, silent = true })
+vim.keymap.set("n", "<Right>", "<Nop>", { noremap = true, silent = true })
+
+-- Disable arrow keys in insert mode
+vim.keymap.set("i", "<Up>", "<Nop>", { noremap = true, silent = true })
+vim.keymap.set("i", "<Down>", "<Nop>", { noremap = true, silent = true })
+vim.keymap.set("i", "<Left>", "<Nop>", { noremap = true, silent = true })
+vim.keymap.set("i", "<Right>", "<Nop>", { noremap = true, silent = true })
+
+-- Disable arrow keys in visual mode
+vim.keymap.set("v", "<Up>", "<Nop>", { noremap = true, silent = true })
+vim.keymap.set("v", "<Down>", "<Nop>", { noremap = true, silent = true })
+vim.keymap.set("v", "<Left>", "<Nop>", { noremap = true, silent = true })
+vim.keymap.set("v", "<Right>", "<Nop>", { noremap = true, silent = true })
+
+vim.api.nvim_set_keymap("n", "<Tab>", ":bnext<CR>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<S-Tab>", ":bprev<CR>", { noremap = true, silent = true })
