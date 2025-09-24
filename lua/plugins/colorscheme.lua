f68bc24b5017b70ffee474ee961593c84a12f166
gustavooarantes <profissional.gustavoa@gmail.com>
2025-09-24 02:17:42 -0300

    changed colorscheme to night-owl

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,19 +1,7 @@
 return {
-  "rose-pine/neovim",
-  name = "rose-pine",
-  lazy = false,
+  "oxfist/night-owl.nvim",
   priority = 1000,
-  opts = {
-    variant = "main", -- options: "auto", "main", "moon", "dawn"
-    dark_variant = "main",
-    bold_vert_split = false,
-    dim_nc_background = false,
-    disable_background = false, -- this makes it transparent
-    disable_float_background = true,
-    disable_italics = false,
-  },
-  config = function(_, opts)
-    require("rose-pine").setup(opts)
-    vim.cmd("colorscheme rose-pine-main")
+  config = function()
+    vim.cmd.colorscheme("night-owl")
   end,
 }
