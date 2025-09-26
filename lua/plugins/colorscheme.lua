ddea56a62a328ad62fad75e64008fae08376fe12
gustavooarantes <profissional.gustavoa@gmail.com>
2025-09-26 15:44:04 -0300

    [feat] Got back to tokyonight night, I have to stop ricing stuff for god's sake

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,7 +1,6 @@
 return {
-  "oxfist/night-owl.nvim",
+  "folke/tokyonight.nvim",
+  lazy = false,
   priority = 1000,
-  config = function()
-    vim.cmd.colorscheme("night-owl")
-  end,
+  opts = {},
 }
