45c710c55fdd7417653d720dcbca6d89ef455b07
gustavooarantes <profissional.gustavoa@gmail.com>
2025-10-02 19:16:48 -0300

    [feat] Configured some of the completion plugins to my taste

---
+++ lua/plugins/completion.lua
@@ -0,0 +1,67 @@
+-- Return plugin specification for Lazy
+return {
+  "hrsh7th/nvim-cmp", -- Main completion plugin
+  dependencies = {
+    "hrsh7th/cmp-nvim-lsp", -- LSP source for nvim-cmp
+    "hrsh7th/cmp-buffer", -- Buffer source for nvim-cmp
+    "hrsh7th/cmp-path", -- Path source for nvim-cmp
+    "L3MON4D3/LuaSnip", -- Snippet engine
+    "rafamadriz/friendly-snippets", -- Predefined snippets collection
+    "saadparwaiz1/cmp_luasnip", -- LuaSnip source for nvim-cmp
+  },
+  config = function()
+    -- Load modules
+    local cmp = require("cmp")
+    local luasnip = require("luasnip")
+
+    -- Load friendly snippets from VSCode collection
+    require("luasnip.loaders.from_vscode").lazy_load()
+
+    -- Setup nvim-cmp
+    cmp.setup({
+      -- Snippet expansion
+      snippet = {
+        expand = function(args)
+          luasnip.lsp_expand(args.body) -- Expand snippet using LuaSnip
+        end,
+      },
+      -- Key mappings for completion menu
+      mapping = {
+        ["<C-u>"] = cmp.mapping.scroll_docs(-4), -- Scroll docs up
+        ["<C-d>"] = cmp.mapping.scroll_docs(4), -- Scroll docs down
+        ["<C-Space>"] = cmp.mapping.complete(), -- Trigger completion manually
+        ["<C-e>"] = cmp.mapping.close(), -- Close completion menu
+        ["<CR>"] = cmp.mapping.confirm({ select = true }), -- Confirm selection
+
+        -- Tab navigation for completion/snippets
+        ["<Tab>"] = cmp.mapping(function(fallback)
+          if cmp.visible() then
+            cmp.select_next_item() -- Select next item in completion menu
+          elseif luasnip.expand_or_jumpable() then
+            luasnip.expand_or_jump() -- Expand or jump in snippet
+          else
+            fallback() -- Default fallback behavior
+          end
+        end, { "i", "s" }),
+
+        ["<S-Tab>"] = cmp.mapping(function(fallback)
+          if cmp.visible() then
+            cmp.select_prev_item() -- Select previous item in completion menu
+          elseif luasnip.jumpable(-1) then
+            luasnip.jump(-1) -- Jump backwards in snippet
+          else
+            fallback() -- Default fallback behavior
+          end
+        end, { "i", "s" }),
+      },
+
+      -- Completion sources
+      sources = {
+        { name = "nvim_lsp" }, -- LSP completions
+        { name = "luasnip" }, -- Snippet completions
+        { name = "buffer" }, -- Buffer completions
+        { name = "path" }, -- File path completions
+      },
+    })
+  end,
+}
