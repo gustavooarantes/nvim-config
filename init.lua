dfdcac13278a7f7df7daefd1e78d39f6869a6280
gustavooarantes <profissional.gustavoa@gmail.com>
2025-10-02 18:39:30 -0300

    [feat] Opens Neovim in Netrw

--- init.lua
+++ init.lua
@@ -1,2 +1,14 @@
--- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
+
+-- Open netrw at startup if no files were passed
+if vim.fn.argc() == 0 then
+  vim.api.nvim_create_autocmd("VimEnter", {
+    callback = function()
+      -- Open netrw
+      vim.cmd("Explore")
+      -- Immediately set relative numbers in the new netrw buffer
+      vim.wo.number = true
+      vim.wo.relativenumber = true
+    end,
+  })
+end
