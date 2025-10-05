646cd83f6c48f1fd44b684be188d704c6603d326
gustavooarantes <profissional.gustavoa@gmail.com>
2025-10-05 10:54:13 -0300

    solarized osaka is the goat

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,6 +1,10 @@
 return {
-  "folke/tokyonight.nvim",
-  lazy = false,
+  "craftzdog/solarized-osaka.nvim",
+  lazy = true,
   priority = 1000,
-  opts = {},
+  opts = function()
+    return {
+      transparent = true,
+    }
+  end,
 }
