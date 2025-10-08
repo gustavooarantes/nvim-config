6343f1f5b7c563816545b8bfcca92ea8a4a31494
gustavooarantes <profissional.gustavoa@gmail.com>
2025-10-08 06:23:29 -0300

    set transparency for the background + other lsp keymaps

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -2,6 +2,11 @@ return {
   "folke/tokyonight.nvim",
   priority = 1000,
   config = function()
+    require("tokyonight").setup({
+      transparent = true,
+      on_colors = function(_) end,
+      on_highlights = function(_, _) end,
+    })
     vim.cmd.colorscheme("tokyonight-night")
   end,
 }
