a4e6bf71a37b6b37354b9873f503ab864964a3a9
gustavooarantes <profissional.gustavoa@gmail.com>
2025-10-14 16:42:32 -0300

    rose-pine

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,12 +1,23 @@
 return {
-  "folke/tokyonight.nvim",
-  priority = 1000,
+  "rose-pine/neovim",
+  name = "rose-pine",
   config = function()
-    require("tokyonight").setup({
-      transparent = true,
-      on_colors = function(_) end,
-      on_highlights = function(_, _) end,
+    require("rose-pine").setup({
+      variant = "main",
+      dark_variant = "main",
+      styles = { bold = true, italic = true, transparency = true },
+      groups = {
+        border = "muted",
+        link = "iris",
+        panel = "surface",
+        error = "love",
+        hint = "iris",
+        info = "foam",
+        note = "pine",
+        todo = "rose",
+        warn = "gold",
+      },
     })
-    vim.cmd.colorscheme("tokyonight-night")
+    vim.cmd("colorscheme rose-pine-main")
   end,
 }
