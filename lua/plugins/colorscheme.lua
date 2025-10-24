cf01f46343b27e372ff245144638ef390a49954c
gustavooarantes <profissional.gustavoa@gmail.com>
2025-10-24 10:27:30 -0300

    better formatting for C/C++ source files

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -2,7 +2,6 @@ return {
 	"ellisonleao/gruvbox.nvim",
 	name = "gruvbox",
 	config = function()
-		vim.o.background = "dark"
 		vim.cmd("colorscheme gruvbox")
 	end,
 }
--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -1,61 +1,61 @@
 return {
-  "stevearc/conform.nvim",
-  opts = {
-    formatters = {
-      my_clang_format = {
-        command = "clang-format",
-        args = {
-          '--style={BasedOnStyle: "Google", IndentWidth: 2, BreakBeforeBraces: "Attach", AllowShortFunctionsOnASingleLine: "None", ColumnLimit: 100}',
-        },
-      },
+	"stevearc/conform.nvim",
+	opts = {
+		formatters = {
+			my_clang_format = {
+				command = "clang-format",
+				args = {
+					'--style={BasedOnStyle: "LLVM", IndentWidth: 2, BreakBeforeBraces: "Attach", AllowShortFunctionsOnASingleLine: "Empty", ColumnLimit: 100}',
+				},
+			},
 
-      my_prettier = {
-        command = "prettier",
-        args = {
-          "--print-width",
-          "80",
-          "--tab-width",
-          "2",
-          "--use-tabs",
-          "false",
-          "--semi",
-          "true",
-          "--single-quote",
-          "false",
-          "--trailing-comma",
-          "es5",
-        },
-      },
+			my_prettier = {
+				command = "prettier",
+				args = {
+					"--print-width",
+					"80",
+					"--tab-width",
+					"2",
+					"--use-tabs",
+					"false",
+					"--semi",
+					"true",
+					"--single-quote",
+					"false",
+					"--trailing-comma",
+					"es5",
+				},
+			},
 
-      my_ruff = {
-        command = "ruff",
-        args = { "format", "--line-length", "88" },
-      },
+			my_ruff = {
+				command = "ruff",
+				args = { "format", "--line-length", "88" },
+			},
 
-      my_shfmt = {
-        command = "shfmt",
-        args = { "-i", "2" },
-      },
-    },
+			my_shfmt = {
+				command = "shfmt",
+				args = { "-i", "2" },
+			},
+		},
 
-    formatters_by_ft = {
-      go = { "gofumpt", "goimports" },
-      dockerfile = { "dockerls" },
-      toml = { "taplo" },
+		formatters_by_ft = {
+			go = { "gofumpt", "goimports" },
+			dockerfile = { "dockerls" },
+			toml = { "taplo" },
 
-      python = { "my_ruff" },
-      sh = { "my_shfmt" },
-      bash = { "my_shfmt" },
-      zsh = { "my_shfmt" },
-      c = { "my_clang_format" },
-      cpp = { "my_clang_format" },
-      lua = { "my_stylua" },
-      html = { "my_prettier" },
-      css = { "my_prettier" },
-      javascript = { "my_prettier" },
-      typescript = { "my_prettier" },
-      json = { "my_prettier" },
-      yaml = { "my_prettier" },
-    },
-  },
+			python = { "my_ruff" },
+			sh = { "my_shfmt" },
+			bash = { "my_shfmt" },
+			zsh = { "my_shfmt" },
+			c = { "my_clang_format" },
+			cpp = { "my_clang_format" },
+			lua = { "my_stylua" },
+			html = { "my_prettier" },
+			css = { "my_prettier" },
+			javascript = { "my_prettier" },
+			typescript = { "my_prettier" },
+			json = { "my_prettier" },
+			yaml = { "my_prettier" },
+		},
+	},
 }
