06a45b34caef489a3e467aee5a036ce544ffe998
gustavooarantes <profissional.gustavoa@gmail.com>
2025-10-26 16:30:15 -0300

    .

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,8 +1,15 @@
 return {
-	"ellisonleao/gruvbox.nvim",
-	name = "gruvbox",
-	config = function()
-		vim.o.background = "dark"
-		vim.cmd("colorscheme gruvbox")
+	"rose-pine/neovim",
+	name = "rose-pine",
+	lazy = false,
+	priority = 1000,
+	opts = {
+		variant = "main",
+		dark_variant = "main",
+		disable_background = true,
+	},
+	config = function(_, opts)
+		require("rose-pine").setup(opts)
+		vim.cmd("colorscheme rose-pine")
 	end,
 }
