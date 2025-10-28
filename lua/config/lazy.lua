1f373052beb571ae43c42c989ba2f3876f3743dc
gustavooarantes <profissional.gustavoa@gmail.com>
2025-10-28 18:57:07 -0300

    Monochromatic colorscheme

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -21,7 +21,7 @@ require("lazy").setup({
 			"LazyVim/LazyVim",
 			import = "lazyvim.plugins",
 			opts = {
-				colorscheme = "gruvbox",
+				colorscheme = "carbonfox",
 			}
 		},
 		-- import/override with your plugins
--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,8 +1,37 @@
 return {
-	"ellisonleao/gruvbox.nvim",
-	name = "gruvbox",
+	"EdenEast/nightfox.nvim",
+	name = "carbonfox",
+	lazy = false,
+	priority = 1000,
 	config = function()
-		vim.o.background = "dark"
-		vim.cmd("colorscheme gruvbox")
+		require("nightfox").setup({
+			options = {
+				transparent = false, -- solid background
+				dim_inactive = false, -- keep all windows same brightness
+				styles = { all = "NONE" },
+			},
+		})
+
+		-- Load Carbonfox first
+		vim.cmd("colorscheme carbonfox")
+		vim.opt.background = "dark"
+
+		-- Apply pitch-black background globally
+		vim.defer_fn(function()
+			local black = "#000000"
+			-- Core windows
+			vim.api.nvim_set_hl(0, "Normal", { bg = black })
+			vim.api.nvim_set_hl(0, "NormalNC", { bg = black })
+			vim.api.nvim_set_hl(0, "SignColumn", { bg = black })
+			vim.api.nvim_set_hl(0, "VertSplit", { bg = black, fg = black })
+			vim.api.nvim_set_hl(0, "LineNr", { bg = black })
+			vim.api.nvim_set_hl(0, "CursorLineNr", { bg = black })
+
+			-- Floating windows / popups
+			vim.api.nvim_set_hl(0, "NormalFloat", { bg = black })
+			vim.api.nvim_set_hl(0, "FloatBorder", { bg = black, fg = black })
+			vim.api.nvim_set_hl(0, "Pmenu", { bg = black })
+			vim.api.nvim_set_hl(0, "PmenuSel", { bg = "#323232" })
+		end, 0)
 	end,
 }
