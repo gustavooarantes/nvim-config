55de5194341f4bdd8c2acfcf7c9d65cf60fbbe2e
gustavooarantes <profissional.gustavoa@gmail.com>
2025-10-31 12:49:07 -0300

    got snacks back (oh god......) and tweaked carbonfox to keep the pitch black background but using the monokai classic pelette for the rest for HC

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,37 +1,80 @@
 return {
-	"EdenEast/nightfox.nvim",
-	name = "carbonfox",
-	lazy = false,
-	priority = 1000,
-	config = function()
-		require("nightfox").setup({
-			options = {
-				transparent = false, -- solid background
-				dim_inactive = false, -- keep all windows same brightness
-				styles = { all = "NONE" },
-			},
-		})
+  "EdenEast/nightfox.nvim",
+  name = "carbonfox",
+  lazy = false,
+  priority = 1000,
+  config = function()
+    require("nightfox").setup({
+      palettes = {
+        carbonfox = {
+          -- Derived from your Monokai Classic palette
+          bg0     = "#272822", -- base background (ignored due to black override)
+          bg1     = "#1d1e19",
+          bg2     = "#161613",
+          fg0     = "#fdfff1", -- text
+          fg1     = "#c0c1b5", -- slightly dimmed
+          fg2     = "#919288", -- dimmed2
+          sel0    = "#3b3c35", -- selection bg
+          sel1    = "#57584f", -- alternate selection
+          comment = "#6e7066", -- dimmed3
 
-		-- Load Carbonfox first
-		vim.cmd("colorscheme carbonfox")
-		vim.opt.background = "dark"
+          -- Monokai Classic accents mapped to Nightfox semantic colors
+          red     = "#f92672", -- accent1
+          pink    = "#f92672",
+          orange  = "#fd971f", -- accent2
+          yellow  = "#e6db74", -- accent3
+          green   = "#a6e22e", -- accent4
+          cyan    = "#66d9ef", -- accent5
+          blue    = "#66d9ef", -- same cyan tone (Monokai uses cyan for both)
+          magenta = "#ae81ff", -- accent6
+          white   = "#fdfff1",
+          black   = "#000000",
+        },
+      },
+      specs = {
+        carbonfox = {
+          syntax = {
+            keyword     = "magenta", -- purple
+            conditional = "magenta",
+            number      = "orange",  -- orange
+            constant    = "orange",
+            string      = "yellow",  -- yellow
+            func        = "green",   -- green
+            statement   = "magenta",
+            operator    = "pink",    -- red/pink
+            preproc     = "cyan",    -- cyan
+            type        = "blue",    -- cyanish blue
+            variable    = "white",   -- text
+            field       = "white",
+            comment     = "comment", -- dim gray
+          },
+        },
+      },
+      options = {
+        transparent = false,
+        dim_inactive = false,
+        styles = { all = "NONE" },
+      },
+    })
 
-		-- Apply pitch-black background globally
-		vim.defer_fn(function()
-			local black = "#000000"
-			-- Core windows
-			vim.api.nvim_set_hl(0, "Normal", { bg = black })
-			vim.api.nvim_set_hl(0, "NormalNC", { bg = black })
-			vim.api.nvim_set_hl(0, "SignColumn", { bg = black })
-			vim.api.nvim_set_hl(0, "VertSplit", { bg = black, fg = black })
-			vim.api.nvim_set_hl(0, "LineNr", { bg = black })
-			vim.api.nvim_set_hl(0, "CursorLineNr", { bg = black })
+    -- Load Carbonfox
+    vim.cmd("colorscheme carbonfox")
+    vim.opt.background = "dark"
 
-			-- Floating windows / popups
-			vim.api.nvim_set_hl(0, "NormalFloat", { bg = black })
-			vim.api.nvim_set_hl(0, "FloatBorder", { bg = black, fg = black })
-			vim.api.nvim_set_hl(0, "Pmenu", { bg = black })
-			vim.api.nvim_set_hl(0, "PmenuSel", { bg = "#323232" })
-		end, 0)
-	end,
+    -- Pitch-black UI overrides (intact)
+    vim.defer_fn(function()
+      local black = "#000000"
+
+      vim.api.nvim_set_hl(0, "Normal", { bg = black })
+      vim.api.nvim_set_hl(0, "NormalNC", { bg = black })
+      vim.api.nvim_set_hl(0, "SignColumn", { bg = black })
+      vim.api.nvim_set_hl(0, "VertSplit", { bg = black, fg = black })
+      vim.api.nvim_set_hl(0, "LineNr", { bg = black })
+      vim.api.nvim_set_hl(0, "CursorLineNr", { bg = black })
+      vim.api.nvim_set_hl(0, "NormalFloat", { bg = black })
+      vim.api.nvim_set_hl(0, "FloatBorder", { bg = black, fg = black })
+      vim.api.nvim_set_hl(0, "Pmenu", { bg = black })
+      vim.api.nvim_set_hl(0, "PmenuSel", { bg = "#323232" })
+    end, 0)
+  end,
 }
--- lua/plugins/explorer.lua
+++ lua/plugins/explorer.lua
@@ -1,47 +1,26 @@
--- Disabling Snacks explorer and dashboard
 return {
-	"folke/snacks.nvim",
-	opts = {
-		explorer = { enabled = false },
-	},
-	config = function()
-		-- Toggle netrw function
-		local function toggle_netrw()
-			local netrw_buf = nil
-			for _, buf in ipairs(vim.api.nvim_list_bufs()) do
-				if vim.bo[buf].filetype == "netrw" and vim.api.nvim_buf_is_loaded(buf) then
-					netrw_buf = buf
-					break
-				end
-			end
-
-			if netrw_buf and vim.api.nvim_buf_is_valid(netrw_buf) then
-				vim.api.nvim_buf_delete(netrw_buf, { force = true })
-			else
-				vim.cmd.Ex()
-			end
-		end
-
-		-- Map leader+e to toggle netrw
-		vim.keymap.set("n", "<leader>e", toggle_netrw, { desc = "Toggle netrw", silent = true })
-
-		-- Enable numbers for netrw buffers
-		vim.api.nvim_create_autocmd("FileType", {
-			pattern = "netrw",
-			callback = function()
-				vim.opt_local.number = true
-				vim.opt_local.relativenumber = true
-			end,
-		})
-
-		-- Open netrw if Vim starts on a directory
-		vim.api.nvim_create_autocmd("VimEnter", {
-			callback = function()
-				local f = vim.fn.expand("<afile>")
-				if vim.fn.isdirectory(f) == 1 then
-					vim.cmd.Lexplore()
-				end
-			end,
-		})
-	end,
+  "folke/snacks.nvim",
+  opts = {
+    picker = {
+      hidden = true,
+      ignored = true,
+      sources = {
+        files = {
+          hidden = true,
+          ignored = true,
+        },
+        explorer = {
+          layout = {
+            preset = "sidebar",
+            preview = false,
+            layout = {
+              position = "right",
+            },
+          },
+          cycle = true,
+          auto_close = true,
+        },
+      },
+    },
+  },
 }
